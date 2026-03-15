450a6cdda6ddad9e30665cc2d3d9d6abc0f3a770
Abhishek Tiwari <tiwariabhi020@gmail.com>
2026-03-15 22:46:53 +0530

    Initial commit

---
+++ Demo.java
@@ -0,0 +1,9 @@
+public class Demo{
+
+    public static void main(String args[])
+    {
+        System.out.println("This is java program");
+        System.out.println("I am from java jenkins");
+
+    }
+}
